7bd936813faf9936bfef3d06f010e6186a9f8b64
Rehan <syed.rehanhaque1994@gmail.com>
2024-05-20 15:43:40 +0530

    add top layout

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardLayout from "./layouts/DashboardLayout";
 import BooksPage from "./pages/BooksPage";
+import AuthLayout from "./layouts/AuthLayout";
 const router = createBrowserRouter([
   {
     path: "dashboard",
@@ -20,15 +21,22 @@ const router = createBrowserRouter([
         }
     ]
   },
+  {
+    path:"/auth",
+    element:<AuthLayout/>,
+    children:[
+        {
+            path: "login",
+            element: <LoginPage />,
+          },
+          {
+            path: "register",
+            element: <RegisterPage />,
+          },
+    ]
+  }
 
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
+  
 ]);
 
 export default router;
