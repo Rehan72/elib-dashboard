11b91ce1a3cd6507592e3afd92447f22904abcac
Rehan <syed.rehanhaque1994@gmail.com>
2024-05-21 23:58:43 +0530

    add login functonality

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+     <QueryClientProvider client={queryClient}>
     <RouterProvider  router={router}/>
+    </QueryClientProvider>
     {/* <App /> */}
   </React.StrictMode>,
 )
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,10 +9,36 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
+import { login } from "@/http/api"
+import { useMutation } from "@tanstack/react-query"
+import { useRef } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 function LoginPage() {
-  
+  const navigate=useNavigate()
+  const emailRef=useRef<HTMLInputElement>(null);
+  const passwordRef=useRef<HTMLInputElement>(null);
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+     console.log("Login successful");
+     navigate('/dashboard/home');
+    },
+
+  })
+
+  const handleLoginSubmit=() => {
+    const email=emailRef?.current?.value;
+    const password=passwordRef?.current?.value;
+ console.log("Data", email, password);
+ 
+    //Make server call
+    if(!email || !password){
+      return alert("Please enter your email and password");
+    }
+    mutation.mutate({email, password});
+  };
     return (
       <section className="flex justify-center items-center h-screen">
         <Card className="w-full max-w-sm">
@@ -25,19 +51,19 @@ function LoginPage() {
         <CardContent className="grid gap-4">
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" required />
+            <Input ref={emailRef} id="email" type="email" placeholder="m@example.com" required />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className="w-full">
-          <Button className="w-full">Sign in</Button>
+          <Button onClick={handleLoginSubmit} className="w-full">Sign in</Button>
           <div className="mt-4 text-center text-sm">
           Already have an account?{" "}
-          <Link to={'/register'} className="underline">
+          <Link to={'/auth/register'} className="underline">
             Sign in
           </Link>
         </div>
